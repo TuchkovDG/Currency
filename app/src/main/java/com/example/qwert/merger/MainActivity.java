75d2357f0a29dba88ef2eb8a0712721ea2fa7a51
DSemk <samik6677@gmail.com>
2016-11-13 23:04:42 +0300

    add Found activity

--- app/src/main/java/com/example/qwert/merger/MainActivity.java
+++ app/src/main/java/com/example/qwert/merger/MainActivity.java
@@ -62,6 +62,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent1 = new Intent(MainActivity.this, FoundCurrenciesActivity.class);
+                intent1.putExtra(SELECTED_CURRENCY, position);
                 startActivity(intent1);
                 return true;
             }
